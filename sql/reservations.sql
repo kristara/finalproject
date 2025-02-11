bab3ef6afa0e07d500464c1f20184929e622ef3f
kristara <krista.racene94@gmail.com>
2025-02-11 00:50:44 +0000

    adjusting form

--- sql/reservations.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE reservations (
-    reservation_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    flight_id INT,
-    status ENUM('reserved', 'confirmed', 'cancelled') DEFAULT 'reserved',
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (flight_id) REFERENCES flights(flight_id)
-);
