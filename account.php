36865bfc835773964825c020d974df6df144f90b
kristara <krista.racene94@gmail.com>
2025-05-09 14:06:21 +0300

    Get all user details

--- account.php
+++ account.php
@@ -8,8 +8,19 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
+$user_id = $_SESSION['user_id'];
+$email = $_SESSION['email'];
 
+// Fetch user details
+$sql = "SELECT first_name, last_name FROM users WHERE user_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
 $conn->close();
+
+
 ?>
 
 <!DOCTYPE html>
